7e7e3f2439fc9c85cfdce5873fd44eb9f4d339b7
Fana Teffera <fteffera@gmail.com>
2016-04-26 09:10:52 -0500

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -7,3 +7,12 @@ read message
 
 git commit -m $message
 
+git log
+
+read -n1 -p "Do you want to push to the branch?" choice 
+case $choice in  
+  y|Y) read -n -p "What is orgin?" branch 
+       git push origin $branch;; 
+  n|N) echo no ;; 
+  *) echo dont know ;; 
+esac
